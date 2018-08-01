1b6dc08fa8d48c81174a1577aa7a3f7034da9558
tyler <tjfr8s@gmail.com>
2018-08-01 09:04:12 -0700

    Test creation of Vampire class.

--- CharacterTest.cpp
+++ CharacterTest.cpp
@@ -1,6 +1,14 @@
 #include "Character.hpp"
+#include "Battle.hpp"
+#include "Vampire.hpp"
 
 int main()
 {
+    Battle* battle = new Battle;
+
+    Vampire vampire(battle);
+
+
+    delete battle;
     return 0;
 }
